1fc124519a70860f6d612fdc0898cb526c414925
pavan <pavan@gmail.com>
2026-09-07 10:08:08 +0000

    java file added

---
+++ app.java
@@ -0,0 +1,17 @@
+public class CircleArea {
+
+    public static void main(String[] args) {
+        // Declare variables for the circle
+        double radius = 5.5;
+        double pi = 3.14159;
+        
+        // Perform the math operation (Area = pi * r^2)
+        double area = pi * radius * radius;
+        
+        // Print the result with context
+        System.out.println("The radius of the circle is: " + radius);
+        System.out.println("The calculated area is: " + area);
+    }
+}
+}
+
