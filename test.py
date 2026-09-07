a5822440352c99a2ddf3e854be2e7f3c64a76c56
pavan-pa <pavanpadamata5555@gmail.com>
2026-09-07 16:30:50 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Webpage</title>
+    <!-- Connects the CSS file -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="card">
+        <h1>Hello World</h1>
+        <p>This box is styled using a CSS class selector.</p>
+        <button class="btn-primary">Click Me</button>
+    </div>
+
+    <footer id="main-footer">
+        <p>&copy; 2026 My Website</p>
+    </footer>
+
+</body>
+</html>
